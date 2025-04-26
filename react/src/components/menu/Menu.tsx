540b79b3831a43699d2c03154bbd0d965b861468
DyakivNazar <nazar2005.03.21@gmail.com>
2025-04-26 19:58:23 +0300

    appdate rep main, add react-router-dom

--- react/src/components/menu/Menu.tsx
+++ react/src/components/menu/Menu.tsx
@@ -4,9 +4,7 @@ const Menu = () => {
     return (
         <div>
             <ul>
-                <li><Link to={'users'}>Users</Link></li>
-                <li><Link to={'posts'}>Posts</Link></li>
-                <li><Link to={'comments'}>Comments</Link></li>
+                <li><Link to={'/'}></Link></li>
             </ul>
         </div>
     );
