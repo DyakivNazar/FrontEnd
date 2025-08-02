9f55f944a4a6b8d5707b91af372e1d61cf6571f2
DyakivNazar <nazar2005.03.21@gmail.com>
2025-08-03 02:06:49 +0300

    add hw7 task1

--- react/src/components/usercomp/UsersComponent.tsx
+++ react/src/components/usercomp/UsersComponent.tsx
@@ -1,5 +1,5 @@
 import UserComponent from "./UserComponent.tsx";
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 import {baseUrlJP} from "../../constants/urls.ts";
 
 const UsersComponent = () => {
@@ -12,9 +12,17 @@ const UsersComponent = () => {
             .then(value => setUsers(value))
     }, []);
 
+    const foo = useCallback(() => {
+        console.log('foo')
+    }, []) // useCallback ця функція, хешує об'єкт або функцію, для того щоб зайвий раз не рірендерився UserComponent
+
+    const arr:number[] = useMemo(() => {
+        return [192, 123, 523, 56];
+    }, []); // useMemo ця функція оптимізує масив, та повертає масив чисел
+
     return (
         <div> users component
-            <UserComponent/>
+            <UserComponent foo={foo} arr={arr}/>
         </div>
     );
 };
