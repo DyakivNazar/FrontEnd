a3982d854d0fedb9d8e8c02a79a694e9eb5dbc93
DyakivNazar <nazar2005.03.21@gmail.com>
2025-09-23 17:03:57 +0300

    hw 8

--- react/src/components/menu/Menu.tsx
+++ react/src/components/menu/Menu.tsx
@@ -4,7 +4,10 @@ const Menu = () => {
     return (
         <div>
             <ul>
-                <li><Link to={'/'}></Link></li>
+                <li><Link to={''}>home</Link></li>
+                <li><Link to={'users'}>users</Link></li>
+                <li><Link to={'posts'}>posts</Link></li>
+                <li><Link to={'comments'}>comments</Link></li>
             </ul>
         </div>
     );
--- react/src/routers/router.tsx
+++ react/src/routers/router.tsx
@@ -1,12 +1,17 @@
 import {createBrowserRouter} from "react-router-dom";
 import MainLayout from "../layouts/MainLayout.tsx";
+import {UsersPage} from "../pages/UsersPages/UsersPage.tsx";
+import {PostsPage} from "../pages/postsPages/PostsPage.tsx";
+import {CommentsPage} from "../pages/CommentsPages/CommentsPage.tsx";
 
 export const router = createBrowserRouter(
     [
         {
             path:'/', element:<MainLayout/>,
             children:[
-
+                {path: 'users', element:<UsersPage/>},
+                {path: 'posts', element:<PostsPage/>},
+                {path: 'comments', element:<CommentsPage/>}
             ]
         }
     ]
